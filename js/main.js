28681f65949eab9c81326f59b9b1b8c635daa999
Asaduzzaman Santo <40804158+asadsanto10@users.noreply.github.com>
2020-07-27 16:19:18 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-// Generate Random Number 
+/*=============== Generate Random Number  =====================*/
 document.getElementById('generateRandomNumberBtn').addEventListener('click', function () { 
     let randomNumber = Math.floor(100000 + Math.random() * 900000);
     document.getElementById('displayNumber').value = randomNumber;
 });
 
-// display number input
+/*================ display number input =================*/
 function numberInput(value) {
     let displayInputNumber = document.getElementById('displayInputNumber').value;
     let numberLenght = displayInputNumber.toString().length;
@@ -13,7 +13,7 @@ function numberInput(value) {
     }
 }
 
-// clear and backspace button function
+/*============== clear and backspace button function ===============*/
 function clearBackspaceButton(buttonCondition){
     if (buttonCondition == '<') {
         let displayInputNumber = document.getElementById('displayInputNumber');
@@ -25,14 +25,40 @@ function clearBackspaceButton(buttonCondition){
     }
 }
 
-// try lefet function
+/*============= try lefet function ==================*/
 var count = 0;
-console.log(count);
-function clicksubmit() {
+document.getElementById('submit').addEventListener('click', function(){
     count += 1;
     document.getElementById('tryLeft').innerHTML = Math.abs(count - 3);
     var btn = document.getElementById('submit');
     if (count >= 3) {
         btn.disabled = true;
     }
+});
+
+// var inputNumber = displayInputNumber = document.getElementById('displayInputNumber').value;
+// if (inputNumber.length === 0) {
+//     console.log('true');
+// }
+
+function success() {
+    if (document.getElementById("displayInputNumber").value === "") {
+        document.getElementById('submit').disabled = true;
+    } else {
+        document.getElementById('submit').disabled = false;
+    }
+}
+
+/*=============== pin match function ================*/ 
+function submit(){
+    var generatePinNumber = document.getElementById('displayNumber').value;
+    var inputNumber = displayInputNumber = document.getElementById('displayInputNumber').value;
+    // if (generatePinNumber == inputNumber){
+    //     document.getElementById('sucessMessage').classList.add('d-block');
+    //     console.log('true');
+    // }
+    // else{
+    //     document.getElementById('wrongMessage').classList.add('d-block');
+    //     console.log('false');
+    // }
 }
